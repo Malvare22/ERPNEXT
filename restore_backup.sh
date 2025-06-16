02e7782169d7d748e4c6c32ec65425cf7b6f89b2
Rodrigo Andrés Malaver Suárez <pingoalex22@hotmail.com>
2025-06-16 09:58:52 -0500

    first commit

---
+++ restore_backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+CONTAINER_NAME=frappe_docker-backend-1
+BACKUP_DIR=./backup
+
+echo "Copiando backups al contenedor..."
+docker cp $BACKUP_DIR/. $CONTAINER_NAME:/home/frappe/frappe-bench/sites/frontend/private/backups/
+
+echo "Ejecutando restore y comandos dentro del contenedor..."
+docker exec -it $CONTAINER_NAME bash -c "
+  bench --site frontend --force restore /home/frappe/frappe-bench/sites/frontend/private/backups/database.sql.gz --with-public-files /home/frappe/frappe-bench/sites/frontend/private/backups/files.tar --with-private-files /home/frappe/frappe-bench/sites/frontend/private/backups/private-files.tar &&
+  tar -xf /home/frappe/frappe-bench/sites/frontend/private/backups/files.tar -C /home/frappe/frappe-bench/sites/frontend/private/backups/ &&
+  tar -xf /home/frappe/frappe-bench/sites/frontend/private/backups/private-files.tar -C /home/frappe/frappe-bench/sites/frontend/private/backups/ &&
+  bench --site frontend migrate &&
+  bench --site frontend clear-cache &&
+  bench --site frontend  set-config server_script_enabled true
+"
+
+echo "Reiniciando la stack..."
+sudo docker compose down
+sudo docker compose -f pwd.yml up -d
+
+echo "¡Restauración y reinicio completados!"
+
